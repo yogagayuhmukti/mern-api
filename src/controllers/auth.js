b9ff2d08d4d5fa6c2b1aa1e3f08b4c28200da665
gm906~ <gayuhmukti906@gmail.com>
2023-04-02 13:50:45 +0700

    registrasi

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,14 +1,49 @@
+const Autho = require(`../models/auth`);
+const { validationResult } = require(`express-validator`);
+
 exports.register = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const err = new Error(`input value tidak sesuai`);
+    err.errorStatus = 400;
+    err.data = errors.array();
+    throw err;
+  }
+
   const name = req.body.name;
   const email = req.body.email;
   const password = req.body.password;
-  const result = {
-    message: "Register Success",
-    data: {
-      uid: 1,
-      name: name,
-      email: email,
-    },
-  };
-  res.status(201).json(result);
+
+  const Registring = new Autho({
+    name: name,
+    email: email,
+    password: password,
+  });
+
+  Registring.save()
+    .then((result) => {
+      res.status(201).json({
+        message: "Authentication Success",
+        data: result,
+      });
+    })
+    .catch((err) => {
+      console.log(`err`, err);
+    });
 };
+
+// exports.register = (req, res, next) => {
+//   const name = req.body.name;
+//   const email = req.body.email;
+//   const password = req.body.password;
+//   const result = {
+//     message: "Register Success",
+//     data: {
+//       uid: 1,
+//       name: name,
+//       email: email,
+//     },
+//   };
+//   res.status(201).json(result);
+// };
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const { body } = require(`express-validator`);
 
 const router = express.Router();
 
 const authController = require(`../controllers/auth`);
 
-router.post(`/register`, authController.register);
+router.post(
+  `/register`,
+  [
+    body(`name`).isLength({ min: 5 }).withMessage(`nama tidak sesuai`),
+    body(`email`).isLength({ min: 5 }).withMessage(`email tidak sesuai`),
+    body(`password`).isLength({ min: 5 }).withMessage(`password tidak sesuai`),
+  ],
+  authController.register
+);
 
 module.exports = router;
